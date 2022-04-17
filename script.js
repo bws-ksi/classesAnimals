c3d695350686365e419e75ddc6d0b6230d6eb818
bws.ksi <bws.ksi@gmail.com>
2022-04-17 22:33:06 +0300

    add files patern

---
+++ script.js
@@ -0,0 +1,62 @@
+class Animals {
+  constructor(){
+    //
+  }
+  move(){}
+  say(){}
+  eat(){}
+}
+
+class Mammals extends Animals{
+  constructor(){
+    //
+  }
+}
+
+class Birds extends Animals{
+  constructor(){
+    //
+  }
+}
+
+class Fish extends Animals{
+  constructor(){
+    //
+  }
+}
+
+class Predators extends Mammals{
+  constructor(){
+    //
+  }
+}
+
+class Whales extends Mammals{
+  constructor(){
+    //
+  }
+}
+
+class Primates extends Mammals{
+  constructor(){
+    //
+  }
+}
+
+class Dog extends Predators{
+  constructor(){
+    //
+  }
+}
+
+class Dolphin extends Whales{
+  constructor(){
+    //
+  }
+}
+
+class Human extends Primates{
+  constructor(){
+    //
+  }
+}
